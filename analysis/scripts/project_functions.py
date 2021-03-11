7d32b1436966410293deb2df978aef60b2202425
rithujaa <rithujaa128@gmail.com>
2021-03-11 16:32:35 +0530

    Added method chain 1

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -7,11 +7,13 @@ def load_and_process(url_or_path_to_csv_file):
 
     # Method Chain 1 (Load data and deal with missing data)
     df1 = (
-          pd.read_csv("../../data/raw/BankCustomers.csv")
-          .rename(...)
-          .dropna()
-          # etc...
-      )
+        pd.read_csv(path)
+          .loc[lambda x: ~x['Marital_Status'].str.contains("Unknown", na=False)]
+          .loc[lambda x: ~x['Income_Category'].str.contains("Unknown", na=False)]
+          .loc[lambda x: ~x['Education_Level'].str.contains("Unknown", na=False)]
+          .reset_index()
+          .drop('index', axis = 1)
+     )
 
     # Method Chain 2 (Create new columns, drop others, and do processing)
 
