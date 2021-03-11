a5b526ce737c5d79a93049432b00c5c0dc775812
Sunvat Singh Brar <sunvat1412@gmail.com>
2021-03-11 18:25:47 +0530

    Updated the .py file with a new function

--- analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
+++ analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -2,3 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+def load_and_process(path):
+
+    # Method Chain 1 (Load data and deal with missing data)
+    df1 = (
+        pd.read_csv(path)
+          .loc[lambda x: ~x['Marital_Status'].str.contains("Unknown", na=False)]
+          .loc[lambda x: ~x['Income_Category'].str.contains("Unknown", na=False)]
+          .loc[lambda x: ~x['Education_Level'].str.contains("Unknown", na=False)]
+          .reset_index()
+          .drop('index', axis = 1)
+     )
+
+
+    # Method Chain 2 (Create new columns, drop others, and do processing)
+    df2 = (df
+        .rename(columns = {'Card_Category':'Card Type','Customer_Age':'Age','Income_Category':'Income','Credit_Limit':'Credit Limit','Education_Level':'Education','Months_Inactive_12_mon':'Months Inactive','Total_Relationship_Count':'Relationship Count'})
+.drop(columns=['Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1','Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2','Total_Ct_Chng_Q4_Q1','Total_Amt_Chng_Q4_Q1']))
+    return df2
--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -15,7 +15,9 @@ def load_and_process(path):
           .drop('index', axis = 1)
      )
 
-    return df1
 
     # Method Chain 2 (Create new columns, drop others, and do processing)
-
+    df2 = (df
+        .rename(columns = {'Card_Category':'Card Type','Customer_Age':'Age','Income_Category':'Income','Credit_Limit':'Credit Limit','Education_Level':'Education','Months_Inactive_12_mon':'Months Inactive','Total_Relationship_Count':'Relationship Count'})
+.drop(columns=['Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1','Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2','Total_Ct_Chng_Q4_Q1','Total_Amt_Chng_Q4_Q1']))
+    return df2
